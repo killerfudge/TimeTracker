b2370c0fa16464d23db4f83e8887c883c4912fdf
killerfudge <trem_fan@yahoo.com>
2023-03-31 21:34:23 -0500

    database setup?

--- library/connections.php
+++ library/connections.php
@@ -1,9 +1,15 @@
 <?php
 function TimeTrackerConnect()
 {
-  $server = 'localhost';
-  $dbname= 'TimeTracker';
-  $userInfo = array('username' => 'IClient', 'password' => 'gBYOzz_F1MaCpUys');
+  $server = 'us-cdbr-east-06.cleardb.net';
+  $dbname = 'heroku_94a3c1b69b07e2e';
+  $userInfo = array('username' => 'bcccd966ba8700', 'password' => '995e5c76');
+  if($_SERVER['HTTP_HOST'] == 'localhost')
+  {
+    $server = 'localhost';
+    $dbname= 'TimeTracker';
+    $userInfo = array('username' => 'IClient', 'password' => 'gBYOzz_F1MaCpUys');
+  }
   $dsn = "mysql:host=$server;dbname=$dbname";
   $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 
